2bb02b0fc893720d2e94f9d41c60217f24e20bb1
xuanhua.hu <xuanhu.hu@busonline.com>
2016-08-16 14:19:31 +0800

    12312312

--- src/main/java/net/busonline/api/quartz/impl/QuartzService.java
+++ src/main/java/net/busonline/api/quartz/impl/QuartzService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
 import net.busonline.api.dao.SignMapper;
 import net.busonline.api.quartz.IQuartzService;
 import redis.clients.jedis.Jedis;
@@ -19,17 +22,21 @@ public class QuartzService implements IQuartzService {
 	@Autowired
 	private SignMapper signMapper;
 
-	@Scheduled(cron = "0/5 * *  * * ?") // 每5秒执行一次
+	//@Scheduled(cron = "0/1 * *  * * ?") // 每5秒执行一次
+	
+	@Scheduled(cron = "0 40 14 * * ?")
 	public void stoptask() {
 		List<Map<String,String>> list =signMapper.startingStop();
+		//this.lpush(0, "GPS", "Y4KLByH0kXgjANdD-tXetg.. 0 39.94272772845409 116.43647494497634 0.0 1470623976000");
 		for(int i = 0 ;i<list.size();i++){
-			//System.out.println(list.get(i).get("lineid")+"_"+list.get(i).get("linedir"));
-			this.seth(2, "stoptask", list.get(i).get("lineid")+"_"+list.get(i).get("linedir"), list.get(i).toString());
+			//JSON.toJSONString(list.get(i));
+			//System.out.println(JSON.toJSONString(list.get(i)));
+			this.seth(2, "stoptask", list.get(i).get("lineid")+"_"+list.get(i).get("linedir"), JSON.toJSONString(list.get(i)));
 		}
-		System.out.println("test executed ...");
+		//System.out.println("test executed ...");
 		List<String> lists = this.hget(2, "stoptask", "668_0");
 		for(int i  = 0 ;i<lists.size() ;i++){
-			System.out.println(lists.get(i));
+			//System.out.println(lists.get(i));
 		}
 	}
 
@@ -43,6 +50,15 @@ public class QuartzService implements IQuartzService {
 		}
 	}
 	
+	public void lpush(int dbIndex, String key,  String value) {
+		Jedis jedis = jedisPool.getResource();
+		try {
+			jedis.select(dbIndex);
+			jedis.lpush(key, value);
+		} finally {
+			jedisPool.returnResource(jedis);
+		}
+	}
 	public List<String> hget(int dbIndex, String key, String sign) {
 
 		Jedis jedis = jedisPool.getResource();
